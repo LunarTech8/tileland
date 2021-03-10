ce0f41aaccdfe5f1b557ef2d3072e58162f3d8c2
LunarTech8 <lunartech8@gmail.com>
2021-03-10 09:47:22 +0100

    Work on camera improvements
    - Also use WASD for camera movement
    - Move camera faster when holding down the shift key
    - Start: "Use scroll wheel to zoom"

--- scripts/keyboard.js
+++ scripts/keyboard.js
@@ -8,15 +8,33 @@ Keyboard.LEFT = 37;
 Keyboard.RIGHT = 39;
 Keyboard.UP = 38;
 Keyboard.DOWN = 40;
+Keyboard.SHIFT = 16;
+Keyboard.W = 87;
+Keyboard.A = 65;
+Keyboard.S = 83;
+Keyboard.D = 68;
 Keyboard._keys = {};
 
-Keyboard.listenForEvents = function(keys)
+Keyboard.onScrolling = function(func)
+{
+    window.addEventListener('wheel', func.bind(this));
+}
+
+Keyboard.listenForKeys = function(keys)
 {
     window.addEventListener('keydown', this._onKeyDown.bind(this));
     window.addEventListener('keyup', this._onKeyUp.bind(this));
-
-    keys.forEach(function(key){this._keys[key] = false;}.bind(this));
+    keys.forEach(function(key){ this._keys[key] = false; }.bind(this));
 }
+
+Keyboard.isDown = function(keyCode)
+{
+    if (!keyCode in this._keys)
+    {
+        throw new Error('Keycode ' + keyCode + ' is not being listened to');
+    }
+    return this._keys[keyCode];
+};
 
 Keyboard._onKeyDown = function(event)
 {
@@ -36,13 +54,4 @@ Keyboard._onKeyUp = function(event)
         event.preventDefault();
         this._keys[keyCode] = false;
     }
-};
-
-Keyboard.isDown = function(keyCode)
-{
-    if (!keyCode in this._keys)
-    {
-        throw new Error('Keycode ' + keyCode + ' is not being listened to');
-    }
-    return this._keys[keyCode];
 };
--- scripts/tiles.js
+++ scripts/tiles.js
@@ -6,12 +6,14 @@ const TILE_ID_TERRAIN_MIN = 1;
 const TILE_ID_TERRAIN_MAX = 3;
 const TILE_ID_VEGETATION_MIN = 4;
 const TILE_ID_VEGETATION_MAX = 6;
-const MAP_TILES_X = 32;  // In tiles
-const MAP_TILES_Y = 32;  // In tiles
+const MAP_TILES_X = 128;  // In tiles
+const MAP_TILES_Y = 128;  // In tiles
 const TILE_SIZE = 64;  // In pixels
 const MAP_SIZE_X = 1536;  // Display size in pixels
 const MAP_SIZE_Y = 768;  // Display size in pixels
-const MAP_PERLIN_NOISE_OCTAVES = 4;
+const MAP_PERLIN_NOISE_OCTAVES = 5;
+const CAMERA_SPEED_NORMAL = 512;  // Pixels per second
+const CAMERA_SPEED_FAST = CAMERA_SPEED_NORMAL * 3;  // Pixels per second
 
 
 // --------------------
@@ -53,7 +55,7 @@ class Map
 
 class Camera
 {
-    static SPEED = 256;  // Pixels per second
+    speed = CAMERA_SPEED_NORMAL;
 
     constructor(map, width, height)
     {
@@ -68,8 +70,8 @@ class Camera
     move(delta, dirX, dirY)
     {
         // Move camera:
-        this.x += dirX * Camera.SPEED * delta;
-        this.y += dirY * Camera.SPEED * delta;
+        this.x += dirX * this.speed * delta;
+        this.y += dirY * this.speed * delta;
         // Clamp values:
         this.x = Math.max(0, Math.min(this.x, this.maxX));
         this.y = Math.max(0, Math.min(this.y, this.maxY));
@@ -94,21 +96,39 @@ Game.init = function()
     this.minY = 0;
     this.maxX = MAP_SIZE_X;
     this.maxY = MAP_SIZE_Y;
-    Keyboard.listenForEvents([Keyboard.LEFT, Keyboard.RIGHT, Keyboard.UP, Keyboard.DOWN]);
+    Keyboard.listenForKeys([Keyboard.LEFT, Keyboard.RIGHT, Keyboard.UP, Keyboard.DOWN, Keyboard.W, Keyboard.A, Keyboard.S, Keyboard.D, Keyboard.SHIFT]);
+    Keyboard.onScrolling(function(event)
+    {
+        event.preventDefault();  // FIXME: check how to prevent browser scrolling
+        console.info(event.deltaY);  // DEBUG: use event.deltaY value to zoom in/out
+    });
     this.tileAtlas = Loader.getImage('tiles');
     this.camera = new Camera(map, this.maxX, this.maxY);
 };
 
 Game.update = function(delta)
 {
-    // Handle camera movement with arrow keys:
     let dirX = 0;
     let dirY = 0;
+    // Handle camera movement with keys:
     if (Keyboard.isDown(Keyboard.LEFT)) { dirX = -1; }
     if (Keyboard.isDown(Keyboard.RIGHT)) { dirX = 1; }
     if (Keyboard.isDown(Keyboard.UP)) { dirY = -1; }
     if (Keyboard.isDown(Keyboard.DOWN)) { dirY = 1; }
+    if (Keyboard.isDown(Keyboard.A)) { dirX = -1; }
+    if (Keyboard.isDown(Keyboard.D)) { dirX = 1; }
+    if (Keyboard.isDown(Keyboard.W)) { dirY = -1; }
+    if (Keyboard.isDown(Keyboard.S)) { dirY = 1; }
+    if (Keyboard.isDown(Keyboard.SHIFT)) { this.camera.speed = CAMERA_SPEED_FAST; } else { this.camera.speed = CAMERA_SPEED_NORMAL; }
     this.camera.move(delta, dirX, dirY);
+};
+
+Game.render = function()
+{
+    // Draw map background layer:
+    this._drawLayer(0);
+    // Draw map top layer:
+    this._drawLayer(1);
 };
 
 Game._drawLayer = function(layer)
@@ -143,12 +163,4 @@ Game._drawLayer = function(layer)
             }
         }
     }
-};
-
-Game.render = function()
-{
-    // Draw map background layer:
-    this._drawLayer(0);
-    // Draw map top layer:
-    this._drawLayer(1);
 };
